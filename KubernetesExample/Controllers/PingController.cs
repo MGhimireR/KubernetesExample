ca0c2da3a05099c188b041273db504c08cd5a555
Raghupathy <ragupathi.kommidi@gmail.com>
2022-03-09 05:33:25 +0530

    Changes

---
+++ KubernetesExample/Controllers/PingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KubernetesExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public PingController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+
+            return Ok("Server is Healthy");
+        }
+    }
+}
